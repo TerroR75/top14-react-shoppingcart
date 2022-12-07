d298186ca3d522a04689a5763cfc13f3f47f9cf1
TerroR75 <kamilsulimierski@gmail.com>
2022-12-07 21:01:51 +0100

    gh-pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'top14-react-shoppingcart',
+  base: '/top14-react-shoppingcart/',
   plugins: [react()],
 });
