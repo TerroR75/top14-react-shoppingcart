b8505f57d4a22d76a748a9701672747538a6d247
TerroR75 <kamilsulimierski@gmail.com>
2022-12-07 21:33:44 +0100

    gh

--- deploy.sh
+++ deploy.sh
@@ -21,7 +21,7 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
+git push -f git@github.com:TerroR75/TerroR75.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:TerroR75/top14-react-shoppingcart.git main:gh-pages
