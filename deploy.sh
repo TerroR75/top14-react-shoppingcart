7699d6f883423b5077e1efb14bf48152f9b0eea9
TerroR75 <kamilsulimierski@gmail.com>
2022-12-07 21:39:57 +0100

    gh

--- deploy.sh
+++ deploy.sh
@@ -24,4 +24,4 @@ git commit -m 'deploy'
 # git push -f git@github.com:terror75/terror75.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:terror75/top14-react-shoppingcart.git main:gh-pages
+git push -f git@github.com:terror75/top14-react-shoppingcart.git main:gh-pages
