f4b8fb5ac41836db41f92cd73f1096509c567406
TerroR75 <kamilsulimierski@gmail.com>
2022-12-12 11:50:08 +0100

    removed placeholder text

--- src/routes/HomePage.jsx
+++ src/routes/HomePage.jsx
@@ -1,7 +1,12 @@
 export default function () {
+  function test() {
+    const aboutUs = document.querySelector('.hp-about-us');
+    console.log(aboutUs);
+  }
+
   return (
     <div className='home-page'>
-      <h1></h1>
+      <div className='hp-about-us'></div>
     </div>
   );
 }
